a5373713c752a3f7cf7b72755aa986546f0bf3e5
NicolasHolmes <nicolasgomes@alunos.fho.edu.br>
2023-09-13 11:06:56 -0300

    Criação Repository

---
+++ APITCC/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using APITCC.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APITCC.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductRequest>> Get();
+        Task<ProductRequest> Get(int id);
+    }
+}
---
+++ APITCC/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using APITCC.Models;
+using APITCC.Models.SQLEntity;
+using APITCC.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APITCC.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _context;
+        public ProductRepository(ProductContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<ProductRequest>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<ProductRequest> Get(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
