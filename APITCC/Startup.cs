617d4b7293509168efefd73e2acd2194621b3b18
NicolasHolmes <nicolasgomes@alunos.fho.edu.br>
2023-09-13 11:07:23 -0300

    Configs gerais

--- APITCC/Startup.cs
+++ APITCC/Startup.cs
@@ -1,7 +1,11 @@
+using APITCC.Models.SQLEntity;
+using APITCC.Repositories;
+using APITCC.Repositories.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,17 +20,20 @@ namespace APITCC
 {
     public class Startup
     {
+        private string _connectionString;
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            _connectionString = Configuration.GetConnectionString("ExtractBot");
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ProductContext>(options => options.UseSqlServer(_connectionString));
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
