431ee4ce4847474a1bce102a03a42c6fc5b0f990
NicolasHolmes <nicolasgomes@alunos.fho.edu.br>
2023-09-13 11:07:12 -0300

    Criação Models

---
+++ APITCC/Models/ProductRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APITCC.Models
+{
+    public class ProductRequest
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public DateTime? MaturityDate { get; set; }
+    }
+}
---
+++ APITCC/Models/SQLEntities/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APITCC.Models.SQLEntity
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ProductRequest> Products { get; set; }
+    }
+}
